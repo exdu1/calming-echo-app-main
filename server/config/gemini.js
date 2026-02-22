e47605ba08f37adb915e578258676e2d2d51bdfa
Eric Du <ericxdu1@gmail.com>
2026-02-22 07:44:13 -0500

    Extracted gemini client initalization

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -1,5 +1,25 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import config from './config/index.js';
 
-let genAI;
-let geminiModel;
+
+
+// Validate API key and initialize Gemini API client
+function geminiClient() {
+  let genAI;
+  let geminiModel;
+  try {
+    const apiKey = config.geminiApiKey;
+    if(!apiKey || apiKey == 'your_gemini_api_key_here') {
+      console.error('Warning: Gemini API key is not configured or is the default value.');
+    } else {
+      genAI = new GoogleGenerativeAI(apiKey);
+      geminiModel = genAI.getGenerativeModel({ model: config.geminiModel});
+      console.log(`Gemini API client initialized with ${config.geminiModel}`);
+    }
+  } catch (error) {
+    console.error('Error initializing Gemini API client:', error.message);
+  };
+};
+
+export default geminiClient;
 
