65ade186a46375e2c67acbffb474931cd891c43c
Eric Du <ericxdu1@gmail.com>
2026-02-22 13:22:25 -0500

    Updated module to export getModel function

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -1,26 +1,21 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import config from './index.js';
 
+let geminiModel = null;
 
 // Validate API key and initialize Gemini API client
-const gemini = function initGemini() {
-  let genAI;
-  let geminiModel;
+export function initGemini() {
   try {
-    const apiKey = config.geminiApiKey;
-    if(!apiKey || apiKey == 'your_gemini_api_key_here') {
-      console.error('Warning: Gemini API key is not configured or is the default value.');
+    const genAI = new GoogleGenerativeAI(apiKey);
+    geminiModel = genAI.getGenerativeModel({ model: config.geminiModel});
+    console.log(`Gemini API client initialized with ${config.geminiModel}`);
+    return true;
+    } catch (error) {
+      console.error('Error initializing Gemini API client:', error.message);
       return false;
-    } else {
-      genAI = new GoogleGenerativeAI(apiKey);
-      geminiModel = genAI.getGenerativeModel({ model: config.geminiModel});
-      console.log(`Gemini API client initialized with ${config.geminiModel}`);
-      return geminiModel;
-    }
-  } catch (error) {
-    console.error('Error initializing Gemini API client:', error.message);
-    return false;
   };
 };
 
-export default gemini;
+export function getModel() {
+  return geminiModel;
+};
