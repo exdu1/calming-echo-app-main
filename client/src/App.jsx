85b564654cb6c0f274fe868758e811a649b3d3b5
Eric Du <ericxdu1@gmail.com>
2026-03-09 17:04:03 -0400

    chore: updated react version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ChatPage from './routes/chatPage/ChatPage';
-import Dashboard from './routes/dashboard/Dashboard';
 import Homepage from './routes/homepage/Homepage';
 import Navbar from './components/Navbar';
 import './app.css';
@@ -14,7 +13,6 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/chat" element={<ChatPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
