e4a5426a906d55ee0ce04bcc461cb70c51b27bb1
Eric Du <ericxdu1@gmail.com>
2026-03-09 20:02:47 -0400

    feature: implemented an error boundary to fail gracefully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ChatPage from './routes/chatPage/ChatPage';
 import Homepage from './routes/homepage/Homepage';
 import Navbar from './components/Navbar';
+import ErrorBoundary from './components/ErrorBoundary';
 import './app.css';
 
 const App = () => {
@@ -10,11 +11,13 @@ const App = () => {
       <Navbar />
       <div className="app-shell">
         <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/chat" element={<ChatPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/chat" element={<ChatPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </ErrorBoundary>
         </main>
       </div>
     </BrowserRouter>
